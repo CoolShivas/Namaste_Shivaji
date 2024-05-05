885c819631017ab1fad044117c8af4eb6ee6cfc1
CoolShivas <shivas2710cool00@gmail.com>
2024-05-06 02:01:04 +0530

    Nested React root div or React.createElement Object

--- App.js
+++ App.js
@@ -4,12 +4,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
 
+
+///////********************************************************************************************************** */
+
+
 // <div id="root2"><div> // // Render the output here;
 // <div id="parent">
 //         <div id="child">
 //             <h1> I am h1 tag. </h1>
 //         </div>
 //  </div>
+
+
+///////********************************************************************************************************** */
 
 // Now converting this nested element into react form or How to write in react form ?
 
@@ -22,3 +29,27 @@ const parent = React.createElement("div", {id : "parent"},
 const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 
 root2.render(parent);
+
+// // React.createElement(object) = Html (Browser understands);
+
+///////********************************************************************************************************** */
+
+
+// <div id="root2"><div> // // Render the output here;
+// <div id="parent">
+//         <div id="child">
+//             <h2> I am h1 tag. </h2>
+//             <h3> I am h2 tag. </h3>
+//         </div>
+//  </div>
+
+
+const gardian = React.createElement("div", {id : "parent"}, 
+    React.createElement("div", {id : "child"}, 
+        [React.createElement("h2", {}, "I am h2 tag."), React.createElement("h3", {}, "I am h3 tag.")]
+    )
+)
+
+const root3 = ReactDOM.createRoot(document.getElementById("root3"));
+
+root3.render(gardian);
